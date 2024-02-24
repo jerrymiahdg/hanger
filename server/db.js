ca6a9df30d8545f3911fa80cb2ba4ae2d4b42edb
jerrymiahdg <kingbebop15@gmail.com>
2024-02-23 18:11:19 -0800

    NEW COMMIT FOR DEPLOYMENT

--- server/db.js
+++ server/db.js
@@ -1,8 +1,30 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("clocatdb", "root", "jeremiah", {
-  host: "localhost",
-  dialect: "mysql",
-});
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  }
+);
+
+// const sequelize = new Sequelize(
+//   "railway",
+//   "root",
+//   "6532E2BF2hA42546EeF5ce-cB2h35GEf",
+//   {
+//     host: "viaduct.proxy.rlwy.net",
+//     dialect: "mysql",
+//   }
+// );
+
+// const sequelize = new Sequelize(
+//   "mysql://root:6532E2BF2hA42546EeF5ce-cB2h35GEf@mysql.railway.internal:3306/railway"
+// );
 
 module.exports = sequelize;
