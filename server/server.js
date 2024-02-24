6d69b86fc1e4ced4b4f3ec1556bcd01d5e85293c
jerrymiahdg <kingbebop15@gmail.com>
2024-02-23 18:39:33 -0800

    session store on mysql commit

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const sequelize = require("./db");
 const Sequelize = require("sequelize");
 const cors = require("cors");
 const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(express.json());
@@ -17,9 +18,12 @@ server.use(
   })
 );
 
+const myStore = new SequelizeStore({ db: sequelize });
+
 server.use(
   session({
     secret: "milpitascalifornia",
+    store: myStore,
     resave: false,
     saveUninitialized: false,
     cookie: { secure: false },
